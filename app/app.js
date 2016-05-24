fdab544c2ac4475776950c6e4c0e2f80f889c923
Alexander Skrabl <alexander@skrabl.de>
2016-05-24 22:58:05 +0100

    added app folder

---
+++ app/app.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+class App extends Component {
+    constructor( props ) {
+        super( props );
+    }
+
+    render() {
+        return (
+            <Text>Hello World</Text>
+        )
+    }
+}
+
+export default App;
