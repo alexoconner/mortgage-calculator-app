399deb64a18c1c8d9527065292191962e63fdfb7
Alexander Skrabl <alexander@skrabl.de>
2016-05-25 22:39:35 +0100

    credits and update of package.json

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,7 @@
 /**
  * Mortgage Calculator App
- * (C) Alexander Skrabl <alexander@skrabl.de>
+ * (C) Alexander Skrabl <alexander@skrabl.de> (http://alexanderskrabl.com)
+ * @Credits: Design & Concept by Kevin Lofthouse (http://lofthou.se/)
  * https://github.com/facebook/react-native
  * @flow
  */
